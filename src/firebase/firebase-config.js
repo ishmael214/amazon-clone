17965fe4d3f104eb2ea323c29edf100323759ecb
ishmael214 <64385652+ishmael214@users.noreply.github.com>
2020-12-18 14:04:42 -0600

    created and styled header component

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVZiC_WDchDJ2Bh-XuLg2MDLN4LLhwMJM",
+    authDomain: "clone-37a95.firebaseapp.com",
+    projectId: "clone-37a95",
+    storageBucket: "clone-37a95.appspot.com",
+    messagingSenderId: "1066447500140",
+    appId: "1:1066447500140:web:16dc93104303eeb2cdf949",
+    measurementId: "G-K9TQWV8Q0G"
+  };
